1fe6142350280e795410609190da987c3f15ce5e
carlos.botero <carlos.boterobedoya@icloud.com>
2025-11-30 11:11:14 -0500

    Delete blog/import turtle.py

--- blog/import turtle.py
+++
@@ -1,7 +0,0 @@
-n=int(input("ingrese cuantos pasos quiere andar la tortuga hacia adelante: "))
-t.forward(n)
-n1= int(input("ingrese cuantos pasos quiere andar la tortuga hacia abajo: "))
-t.right(n1)
-t.forward(n)
-turtle.done()
-    
